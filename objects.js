82662b7c9b90ec8dd43614587bbcd2670176b0c6
Shaun Tung <shaun.tung@gmail.com>
2018-01-20 02:33:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,2 +1,1 @@
-Assign an object to the variable playlist and initialize the object with a key-value pair — the keys will be artist names and the values will be song titles. (What limitation does this impose on our playlist?)
 var playlist = 
