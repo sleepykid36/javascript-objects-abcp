a36e5c421f0014d7a5b649637fe57cb1e807433d
Shaun Tung <shaun.tung@gmail.com>
2018-01-20 02:43:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,6 +5,6 @@ function updatePlaylist(obj, key, value) {
   return obj
 }
 
-function removeFromPlaylist(obj, key, value) {
-  return Object.assign({}, obj, { [key]: value })
+function removeFromPlaylist(obj, value) {
+  delete obj.value
 }
